de45986d7f712fe187d6f46250c41206e695eae6
yapanss <yapanss@yahoo.fr>
2019-07-28 21:05:57 +0000

    refactor register controller after PR review

--- controllers/register.controller.js
+++ controllers/register.controller.js
@@ -1,42 +1,35 @@
-const UserModel = require('../models/user.model');
+const UserModel = require('../model/user.model');
 
 module.exports = async (req, res) => {
   try {
-    const user = new UserModel(req.body);
-    const savedUser = await user.save();
+    const savedUser = await UserModel.create(req.body);
     if (savedUser) {
-      res.json({
-        success: true,
-        message: `Bravo, you have been successfuly registered!`
-      });
-    } else {
-      res.json({
-        success: false,
-        message: `Failed to register user for unknown reasons !`
-      });
+      res.json({ message: 'Bravo, you have been successfuly registered!' });
     }
   } catch (err) {
     let errorData;
     if (err.name == 'MongoError' && err.code == 11000) {
       if (err.errmsg.includes('username')) {
         errorData = {
-          success: false,
           field: 'username',
           message: 'This username already exists. Please choose another one'
         };
       }
       if (err.errmsg.includes('email')) {
         errorData = {
-          success: false,
           field: 'email',
           message: 'This email already exists !'
         };
       }
-    } else {
-      console.log(err.name);
-      errorData = { success: false, message: 'oops, an error occured !' };
-    }
 
-    res.json(errorData);
+      res.status(400).json(errorData);
+    } else if (err.name == 'ValidationError') {
+      errorData = { message: 'Failed to validate your inputs !' };
+      res.status(400).json(errorData);
+    } else {
+      res
+        .status(500)
+        .json({ message: 'Failed to register user for unknown reasons !' });
+    }
   }
 };
