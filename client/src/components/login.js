94f3fb7b2098c14a3c5d46303cf915393a794412
lunify <51797974+lunify@users.noreply.github.com>
2019-07-30 13:13:44 +0100

    finished basic login authentication

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { auth } from '../services/api';
 
-function Login() {
+function Login({ onSubmit }) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
 
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
+      {error && <p>An error occured: {error}</p>}
+
       <label htmlFor="email">Email Address</label>
       <input
         onChange={e => setEmail(e.target.value)}
@@ -31,6 +35,16 @@ function Login() {
       <Link to="/register">Register instead</Link>
     </form>
   );
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const { error, user } = await auth.login({ email, password });
+
+    if (error) {
+      return setError(error);
+    }
+    onSubmit(user);
+  }
 }
 
 export default Login;
