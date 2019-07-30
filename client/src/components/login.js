1f6baedd5a9f7cf7e13012a77e8c32a1c8906b97
lunify <51797974+lunify@users.noreply.github.com>
2019-07-30 13:34:16 +0100

    added styling for Login

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -2,38 +2,56 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { auth } from '../services/api';
 
+import './login.css';
+
 function Login({ onSubmit }) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
 
   return (
-    <form onSubmit={handleSubmit}>
-      {error && <p>An error occured: {error}</p>}
+    <div className="login">
+      <div className="login__inner">
+        <header>
+          <h2 className="login__title">Welcome back !</h2>
+          <p className="login__welcome">We're happy to see you again :)</p>
+          {error && <p className="login__error">An error occured: {error}</p>}
+        </header>
 
-      <label htmlFor="email">Email Address</label>
-      <input
-        onChange={e => setEmail(e.target.value)}
-        value={email}
-        type="email"
-        name="email"
-        id="email"
-        required
-      />
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="email">Email Address</label>
+            <input
+              onChange={e => setEmail(e.target.value)}
+              value={email}
+              type="email"
+              name="email"
+              id="email"
+              autoComplete="username"
+              required
+            />
+          </div>
 
-      <label htmlFor="password">Password</label>
-      <input
-        onChange={e => setPassword(e.target.value)}
-        value={password}
-        type="password"
-        name="password"
-        id="password"
-        required
-      />
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input
+              onChange={e => setPassword(e.target.value)}
+              value={password}
+              type="password"
+              name="password"
+              id="password"
+              autoComplete="current-password"
+              required
+            />
+          </div>
 
-      <button type="submit">Login</button>
-      <Link to="/register">Register instead</Link>
-    </form>
+          <button>Sign In</button>
+          <p className="login__redirect">
+            Need an account ? <Link to="/register">Register</Link> instead
+          </p>
+        </form>
+      </div>
+    </div>
   );
 
   async function handleSubmit(event) {
