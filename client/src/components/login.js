e940bcb5b0c48967b7b4a27a3fc2e23a8f612aee
lunify <51797974+lunify@users.noreply.github.com>
2019-07-30 13:03:29 +0100

    Added login component

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 function Login() {
-  return <div>Login here</div>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <form>
+      <label htmlFor="email">Email Address</label>
+      <input
+        onChange={e => setEmail(e.target.value)}
+        value={email}
+        type="email"
+        name="email"
+        id="email"
+        required
+      />
+
+      <label htmlFor="password">Password</label>
+      <input
+        onChange={e => setPassword(e.target.value)}
+        value={password}
+        type="password"
+        name="password"
+        id="password"
+        required
+      />
+
+      <button type="submit">Login</button>
+      <Link to="/register">Register instead</Link>
+    </form>
+  );
 }
 
 export default Login;
