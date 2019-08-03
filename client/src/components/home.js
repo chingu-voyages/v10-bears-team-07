14c3348330a8c1cab29eadc7e62f982189492e70
lunify <51797974+lunify@users.noreply.github.com>
2019-08-03 04:26:23 +0100

    styled landing page

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import './home.css';
+
 function Home({ isLoggedIn }) {
   return (
-    <div>
+    <div className="landing">
       {/* Replace this with a proper icon */}
       <div
         style={{
@@ -14,19 +16,25 @@ function Home({ isLoggedIn }) {
         }}
       ></div>
 
-      <div>
-        <h1>assName="landing__title">Chat about what matters to you</h1>
-        <p>
+      <div className="landing__content">
+        <h1 className="landing__title">Chat about what matters to you</h1>
+        <p className="landing__lead">
           Real-time text chat that emphasizes theme based channels just like
           IRC, but with discoverability!
         </p>
 
         {isLoggedIn ? (
-          <Link to="/dashboard">Open your dashboard</Link>
+          <Link className="landing__btn" to="/dashboard">
+            Open your dashboard
+          </Link>
         ) : (
           <>
-            <Link to="/register">Register</Link>
-            <Link to="/login">Sign in</Link>
+            <Link className="landing__btn--alt" to="/register">
+              Register
+            </Link>
+            <Link className="landing__btn landing__login" to="/login">
+              Sign in
+            </Link>
           </>
         )}
       </div>
