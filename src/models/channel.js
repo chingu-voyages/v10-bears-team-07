735567b436841957e1db329e23957994a2fdbd01
lunify <51797974+lunify@users.noreply.github.com>
2019-08-09 16:02:50 +0100

    fixed and tweaked channel model

--- src/models/channel.js
+++ src/models/channel.js
@@ -1,24 +1,30 @@
 const mongoose = require('mongoose');
 const ObjectId = mongoose.Schema.Types.ObjectId;
 
-const ChannelSchema = mongoose.Schema(
+const channelSchema = new mongoose.Schema(
   {
-    name: {
-      type: String,
-      required: [true, 'The channel name is required !'],
-      index: { unique: true },
-      minlength: [2, 'The channel name must contain at least 2 characters !']
-    },
     ownerId: {
       type: ObjectId,
       required: true
     },
-    tags: [String],
+    name: {
+      type: String,
+      trim: true,
+      required: [true, 'The channel name is required !'],
+      index: { unique: true },
+      minlength: [3, 'The channel name must contain at least 3 characters !']
+    },
+    description: {
+      type: String,
+      trim: true,
+      maxLength: [150, 'The description must contain at most 150 characters']
+    },
+    tags: [{ type: String, trim: true }],
     members: [ObjectId],
     messages: [
       {
         authorId: ObjectId,
-        text: String,
+        text: { type: String, trim: true },
         createdAt: {
           type: Date,
           default: Date.now
@@ -29,4 +35,4 @@ const ChannelSchema = mongoose.Schema(
   { timestamps: true }
 );
 
-module.exports = mongoose.model('ChannelModel', ChannelSchema);
+module.exports = mongoose.model('channel', channelSchema);
