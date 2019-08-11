c5b3480b769ef3868e71f4cad7f4a1e335036d31
lunify <51797974+lunify@users.noreply.github.com>
2019-08-11 09:10:32 +0100

    handle channel search in dashboard

--- client/src/components/dashboard.js
+++ client/src/components/dashboard.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { channels } from '../services/api';
 
 import './dashboard.css';
 
 function Dashboard() {
+  const [fetchedChannels, setChannels] = useState([]);
+  const [searchKeyword, setKeyword] = useState('');
+  const [error, setError] = useState(null);
+
   return (
     <div className="dashboard">
       <p>Welcome to your dashboard ! Go ahead and...</p>
+      {error && <p>An error occured: {error}</p>}
       <div>
         <input
           className="searchInput"
+          onKeyPress={e => e.key === 'Enter' && searchChannels()}
+          onChange={e => setKeyword(e.target.value)}
+          value={searchKeyword}
           type="text"
           placeholder="Find existing channels"
         />{' '}
@@ -20,6 +29,15 @@ function Dashboard() {
       </div>
     </div>
   );
+
+  async function searchChannels() {
+    const result = await channels.getChannels(searchKeyword);
+    if (result.error) {
+      return setError(result);
+    }
+
+    setChannels(result.channels);
+  }
 }
 
 export default Dashboard;
