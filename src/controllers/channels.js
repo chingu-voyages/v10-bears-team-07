718db588168b4e1392aceba574ac79e04aa8c747
yapanss <yapanss@yahoo.fr>
2019-08-12 16:47:10 +0000

    route end point and controller for leaving_channel

--- src/controllers/channels.js
+++ src/controllers/channels.js
@@ -1,4 +1,4 @@
-const ChannelModel = require('../models/channel');
+const ChannelModel = require("../models/channel");
 
 module.exports = { create, getUserChannels, findByNameMatch, addNewMember };
 
@@ -6,36 +6,35 @@ async function create(req, res) {
   const existingChannel = await ChannelModel.findOne({ name: req.body.name });
 
   if (existingChannel) {
-    return res.json({ error: 'The channel already exists.' });
+    return res.json({ error: "The channel already exists." });
   }
 
   try {
     const channel = await ChannelModel.create(req.body);
     res.json({ channel });
   } catch (err) {
-    res.status(500).json({ error: 'Failed to create channel !' });
+    res.status(500).json({ error: "Failed to create channel !" });
   }
 }
 
 async function getUserChannels(req, res) {
   try {
-    const channels = await ChannelModel.find().or(
-      { ownerId: req.params.id },
-      { members: req.params.id }
-    );
+    const channels = await ChannelModel.find({
+      $or: [{ ownerId: req.params.id }, { members: req.params.id }]
+    });
     res.json({ channels });
   } catch (err) {
-    res.status(500).json({ error: 'Failed to retrieve channels' });
+    res.status(500).json({ error: "Failed to retrieve channels" });
   }
 }
 
 async function findByNameMatch(req, res) {
-  const nameMatch = new RegExp(req.query.keyword, 'i');
+  const nameMatch = new RegExp(req.query.keyword, "i");
 
   try {
     var channels = await ChannelModel.find({ name: nameMatch }).lean();
   } catch (error) {
-    return res.status(500).json({ error: 'internal server error' });
+    return res.status(500).json({ error: "internal server error" });
   }
 
   return res.json({ channels });
@@ -43,13 +42,16 @@ async function findByNameMatch(req, res) {
 
 async function addNewMember(req, res) {
   try {
+    const action = req.params.action;
     const channel = await ChannelModel.findOneAndUpdate(
       { _id: req.params.channelId },
-      { $addToSet: { members: req.params.userId } },
+      action === "join"
+        ? { $addToSet: { members: req.params.userId } }
+        : { $pull: { members: req.params.userId } },
       { new: true }
     );
     res.send(channel);
   } catch (error) {
-    res.status(500).json({ error: 'Failed to retrieve channels' });
+    res.status(500).json({ error: "Failed to retrieve channels" });
   }
 }
