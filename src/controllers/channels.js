cbbb0084a2c285f6d43a47489e27ddb9512ef254
yapanss <yapanss@yahoo.fr>
2019-08-12 14:16:11 +0000

    join_channel backend implementation

--- src/controllers/channels.js
+++ src/controllers/channels.js
@@ -1,4 +1,4 @@
-const ChannelModel = require('../models/channel');
+const ChannelModel = require("../models/channel");
 
 module.exports = { create, getUserChannels, findByNameMatch, addNewMember };
 
@@ -6,14 +6,14 @@ async function create(req, res) {
   const existingChannel = await ChannelModel.findOne({ name: req.body.name });
 
   if (existingChannel) {
-    return res.json({ error: 'The channel already exists.' });
+    return res.json({ error: "The channel already exists." });
   }
 
   try {
     const channel = await ChannelModel.create(req.body);
     res.json({ channel });
   } catch (err) {
-    res.status(500).json({ error: 'Failed to create channel !' });
+    res.status(500).json({ error: "Failed to create channel !" });
   }
 }
 
@@ -25,20 +25,20 @@ async function getUserChannels(req, res) {
     ]);
     res.json({ channels });
   } catch (err) {
-    res.status(500).json({ error: 'Failed to retrieve channels' });
+    res.status(500).json({ error: "Failed to retrieve channels" });
   }
 }
 
 async function findByNameMatch(req, res) {
   const { userId, keyword } = req.query;
-  const nameMatch = new RegExp(keyword, 'i');
+  const nameMatch = new RegExp(keyword, "i");
 
   try {
     var channels = await ChannelModel.find({ name: nameMatch })
       .nor([{ ownerId: userId }, { members: userId }])
       .lean();
   } catch (error) {
-    return res.status(500).json({ error: 'internal server error' });
+    return res.status(500).json({ error: "internal server error" });
   }
 
   return res.json({ channels });
@@ -53,6 +53,6 @@ async function addNewMember(req, res) {
     );
     res.json({ channel });
   } catch (error) {
-    res.status(500).json({ error: 'Failed to retrieve channels' });
+    res.status(500).json({ error: "Failed to retrieve channels" });
   }
 }
